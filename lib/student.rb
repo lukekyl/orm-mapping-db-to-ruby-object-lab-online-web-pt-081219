b58baac8c2cea32ef1f26d484b5acef7d46e42f0
Kyle Luke <th.oldschool@gmail.com>
2019-10-24 02:34:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,6 +73,17 @@ class Student
     end
   end
 
+  def self.all_students_below_12th_grade
+    sql = <<-SQL
+            SELECT *
+            FROM students
+            WHERE grade = 9
+            LIMIT 1
+          SQL
 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
 end
