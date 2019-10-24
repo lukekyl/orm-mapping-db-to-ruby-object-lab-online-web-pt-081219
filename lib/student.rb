9ed7dd356ab76faea3eff61789365cbf4a5f4661
Kyle Luke <th.oldschool@gmail.com>
2019-10-24 02:37:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(X)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
             SELECT *
             FROM students
